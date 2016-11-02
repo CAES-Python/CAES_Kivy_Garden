0e621c87770209329014dc2a1c9d3a436c70d1a4
marcusah <makasuh@gmail.com>
2016-11-02 16:33:07 -0600

    Delete example.py

--- garden.multi_light_indicator/example.py
+++
@@ -1,8 +0,0 @@
-#example
-from kivy.base  import  runTouchApp
-from kivy.lang  import  Builder
-from kivy.garden.multi_light_indicator import  Multi_light_indicator
-from kivy.uix.button import Button
-
-# LOAD KV UIX
-runTouchApp(Builder.load_file('example.kv'))
